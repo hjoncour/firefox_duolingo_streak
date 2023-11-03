e09f67abca9a04dda49caee30cf3425c248585b1
SanteauX <hugo.joncour.k2o@gmail.com>
2023-11-03 01:01:31 -0400

    feature/add-streak.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './styles/App.css';
+import Streak from './components/Streak';
 
 function App() {
   return (
     <div className="App">
       <h1>Duolingo Streak</h1>
+      {/* Render the Streak component */}
+      <Streak />
     </div>
   );
 }
--- src/components/streak.tsx
+++ src/components/streak.tsx
@@ -1,10 +1,44 @@
+import React, { useState, useEffect } from 'react';
 
-const Streak: React.FC<{ image: string }> = ({ image }) => {
+const Streak = () => {
+  const [streakData, setStreakData] = useState<string | null>(null); // Data can be a string or null
+  const [isLoading, setIsLoading] = useState<boolean>(true); // Explicit boolean type
+  const [error, setError] = useState<string | null>(null); // Error can be a string or null
 
+  useEffect(() => {
+    chrome.runtime.sendMessage(
+      { message: "fetchDuolingoData" },
+      (response: { error: React.SetStateAction<string | null>; html: string; }) => {
+        if (response.error) {
+          setError(response.error); // Error is a string
+          setIsLoading(false);
+        } else {
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(response.html, "text/html");
+          const streakElement = doc.querySelector('span[data-test="streak-stat"]');
+          if (streakElement && streakElement.textContent) {
+            setStreakData(streakElement.textContent);
+          } else {
+            setError('Streak element not found or contains no text');
+          }
+          setIsLoading(false);
+        }
+      }
+    );
+  }, []);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
 
   return (
-    <div>Test</div>
-
+    <div className="streak">
+      <h2>Your Streak:</h2>
+      {streakData !== null ? (
+        <p>Current Streak: {streakData}</p>
+      ) : (
+        <p>No streak data available.</p>
+      )}
+    </div>
   );
 };
 
